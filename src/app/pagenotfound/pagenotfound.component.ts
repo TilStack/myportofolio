5d049547adb74dd4581095d8a06b3711ba440147
TilStack <israel01tientcheu@gmail.com>
2024-05-22 22:38:30 +0100

    not found page updated

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-pagenotfound',
   templateUrl: './pagenotfound.component.html',
-  styleUrls: ['./pagenotfound.component.scss']
+  styleUrls: ['./pagenotfound.component.scss','./pagenotfound.component-responsive.scss']
 })
 export class PagenotfoundComponent implements OnInit {
-
-  constructor() { }
+  faArrowRight=faArrowRight
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  goBack(){
+    this.router.navigate(['/home']);
+  }
 }
