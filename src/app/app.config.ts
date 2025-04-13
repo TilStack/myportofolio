e7c72c20cd0eab40cc06d234181ad816544b531c
TilStack <israel01tientcheu@gmail.com>
2025-04-13 01:02:03 +0100

    refactor app configuration for translation support and remove custom loader

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,47 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import {
+  MODE_STORAGE_SERVICE,
+  ModeLocalStorageService,
+} from 'src/styles/storage.service';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { TranslateLoader } from '@ngx-translate/core';
+import { provideHttpClient, HttpClient, withFetch } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule } from '@ngx-translate/core';
+import { ModeToggleService } from 'src/styles/mode.service';
+
+export function httpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ]),
+    ModeToggleService,
+    {
+      provide: MODE_STORAGE_SERVICE,
+      useClass: ModeLocalStorageService,
+    },
   ],
 };
